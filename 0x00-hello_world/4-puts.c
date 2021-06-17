9103940ed4695781bae61b61a1eea9b61761821e
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-17 18:12:34 +0000

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char str1[50];
+strcpy(str1, "Programming is like building a multilingual puzzle");
+puts(str1);
+puts(str2);
+return (0);
+}
