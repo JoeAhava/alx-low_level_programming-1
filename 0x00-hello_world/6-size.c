643e3469f44bba79f4788ce3f2031ee6b0b78463
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-17 18:47:45 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ char c;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %d \n" sizeof(c));
-printf("Size of an int: %d \n" sizeof(i));
-printf("Size of a long int: %d \n" sizeof(li));
-printf("Size of a long long int: %d \n" sizeof(lli));
-printf("Size of a float: %d \n" sizeof(f));
+printf("Size of a char: %d \n", sizeof(c));
+printf("Size of an int: %d \n", sizeof(i));
+printf("Size of a long int: %d \n", sizeof(li));
+printf("Size of a long long int: %d \n", sizeof(lli));
+printf("Size of a float: %d \n", sizeof(f));
 return (0);
 }
