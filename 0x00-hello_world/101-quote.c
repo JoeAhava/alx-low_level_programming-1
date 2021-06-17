1fcc17f7507c53509561213347ad2d49f28ebf17
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-17 19:28:14 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  *    * Return: Always 0 (Success)
  *     *
  */
-
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (0);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
