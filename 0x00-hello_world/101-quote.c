b1aa0acfacf3968c71d587a85830671cd7410ac4
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-17 19:25:08 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
  *  * main - Entry point
-  */
+ *   *
+ *    * Return: Always 0 (Success)
+ *     *
+ */
+
 int main(void)
 {
-makeItSo("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\" \n");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (0);
 }
