9e8e08f78673e400a5c5ea8fc4a053c44a821ee7
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-18 15:59:39 +0000

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     **/
+int main(void)
+{
+	char n = '0';
+	char s = 'a';
+
+	for (; n <= '9' || s <= 'f'; n++)
+	{
+		if (n <= 9)
+			putchar(n);
+		else
+			putchar(s);
+			s++;
+	}
+	putchar(10);
+	return (0);
+}
