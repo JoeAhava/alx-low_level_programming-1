f1113a97a3f6bda2a99dbb77abfae0e154a91623
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-18 14:57:54 +0000

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     **/
+int main(void)
+{
+char all;
+	for (all = 'a'; all <= 'z'; all++)
+	{
+		putchar(all);
+	}
+	return (0);
+}
