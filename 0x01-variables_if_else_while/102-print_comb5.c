2610435cfc2a62af2ae5ebc240dce6790078ba55
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-18 18:27:30 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				for (; d <= '9'; d++)
 				{
-					if ((a < '0') || (a == '0' && b < d))
+					if ((a < c) || (a == c && b < d))
 					{
 						putchar(a);
 						putchar(b);
