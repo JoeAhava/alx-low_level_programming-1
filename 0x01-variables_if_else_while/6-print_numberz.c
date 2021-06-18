09d0094d02410ffb2d22043dbae7b5541e9c6f4d
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-18 15:39:32 +0000

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  *     *     *     **/
 int main(void)
 {
-	        int a;
+	int a;
 
-		        for (a = '0'; a <= '9'; a++)
-				        {
-						                putchar(a);
-								        }
-			        putchar(10);
-				        return (0);
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
 }
