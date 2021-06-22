ebd1012d561dbb3365fab508302542b3b4c8e79f
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 17:13:14 +0000

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Checkes sighnes of numbers
+ *
+ * @n: int value
+ *
+ * Return: returnes the answer
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
