2874bc1f3466b4156b2704cf311a3f4ff0428016
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 18:33:01 +0000

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - adds two intigers
+ *
+ * @a: intiger one
+ *
+ * @b: intiger two
+ *
+ * Return: return value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
