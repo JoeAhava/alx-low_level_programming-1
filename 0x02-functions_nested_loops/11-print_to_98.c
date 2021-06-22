7267ffbea6d0fc74b88df68eb353e4d699b6a2af
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 18:49:56 +0000

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,17 @@ void print_to_98(int n)
 	n = 0;
 	while (n <= 98)
 	{
-		_putchar(n);
+		if (n < 10)
+		{
+			_putchar('0' + (n % 10));
+		}
+		else
+		{
+			_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+		}
+		_putchar(',');
+		_putchar(' ');
 		n++;
 	}
 }
