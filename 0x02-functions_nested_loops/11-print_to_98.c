81436dff19c11b98d25ee5ec87172398eebf1afe
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 19:18:36 +0000

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,12 @@ void print_to_98(int n)
 		printf("%d\n", n);
 	while (n < 98 || n > 98)
 	{
-		if (n > 98)
+		if (n >= 98)
 		{
 			printf("%d, ", n);
 			n--;
-			printf("\n");
 		}
-		else if (n < 98)
+		else if (n <= 98)
 		{
 			printf("%d, ", n);
 			n++;
