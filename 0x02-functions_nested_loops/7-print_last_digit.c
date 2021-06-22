0975baf13d13b025573d150fdbbd62f483664c7a
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 17:36:35 +0000

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @a: intiger value of function
+ *
+ * Return: return type of a funtion
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
