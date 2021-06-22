738c142b1c5a81fe1abd46647b7b782bea4e7920
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 19:40:20 +0000

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,27 +2,4 @@
 
 void print_times_table(int n)
 {
-	int i = 0;
-	int j;
-	while (i < 10)
-	{
-		j = 0;
-		while (j < 10)
-		{
-			if (((i * j) / 10) > 0)
-			                                _putchar('0' + ((i * j) / 10));
-			                        else if (j > 0)
-			                                _putchar(' ');
-			
-			                        _putchar('0' + ((i * j) % 10));
-			                        if (j < 9)
-			                        {
-						                                _putchar(',');
-						                                _putchar(' ');
-						                        }
-			                        j++;
-			                }
-	                _putchar('\n');
-	                i++;
-	        }
 }
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: return value
+ */
+
+int main(void)
+{
+	int n = 0;
+	int sum = 0;
+
+	while (n < 1024)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			sum = sum + n;
+		n++;
+	}
+	return (sum);
+}
