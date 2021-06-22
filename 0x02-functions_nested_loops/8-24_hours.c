9b3a466f28c76701f05fff04ea075c59ac8b2bd9
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 17:57:17 +0000

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "hoberton.h"
+
+/**
+ * jack_bauer - counts time
+ *
+ * Return: has no return
+ */
+
+void jack_bauer(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a <= 23)
+	{
+		b = 0;
+		while (b <= 60)
+		{
+			_putchar(a ':' b '\n');
+			b++;
+		}
+		a++;
+	}
+}
