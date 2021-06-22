a79ba1daae3784e60d6576d23b2d14be25d0e2f2
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 17:22:49 +0000

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _abs - checks the absolute value
+ *
+ *@int: intiger value
+ *
+ * Return: returns the value of _abs
+ */
+
+int _abs(int)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}
