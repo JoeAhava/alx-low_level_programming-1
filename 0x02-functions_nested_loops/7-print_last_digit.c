803b174e3ad383db5a18caa80c99581b1842bfb1
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 17:47:24 +0000

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int a)
 {
 	if (a % 10 == 0)
 	{
-		_putchar('0' + (n % 10));
-		return (n % 10);
+		_putchar('0' + (a % 10));
+		return (a % 10);
 	}
 	else
 	{
-		_putchar('0' + (-1 * (n % 10)));
-		return ((-1 * (n % 10)));
+		_putchar('0' + (-1 * (a % 10)));
+		return ((-1 * (a % 10)));
 	}
 }
