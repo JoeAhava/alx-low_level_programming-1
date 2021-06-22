82140f57fa1d3e05ec649ba564b9779d6c650a06
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 18:41:36 +0000

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,17 @@ void times_table(void)
 		j = 0;
 		while (j < 10)
 		{
-			_putchar('0' + ((i * j) / 10));
+			if (((i * j) / 10) > 0)
+				_putchar('0' + ((i * j) / 10));
+			else if (j > 0)
+				_putchar(' ');
+
+			_putchar('0' + ((i * j) % 10));
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			j++;
 		}
 		_putchar('\n');
