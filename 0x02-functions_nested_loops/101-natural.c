972733c48263b0d19c6eeb45fb2d990a558b4e9e
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 20:01:48 +0000

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,8 +13,13 @@ int main(void)
 
 	while ((n * 3) < 1024 || (n * 5) < 1024)
 	{
-		sum = sum + (n * 3);
-		sum = sum + (n * 5);
+		if ((n * 3) < 1024 && (n * 5) < 1024)
+		{
+			sum = sum + (n * 3);
+			sum = sum + (n * 5);
+		}
+		else if ((n * 3) < 1024 && (n * 5) > 1024)
+			sum = sum + (n * 3);
 		n++;
 	}
 	printf("%ld", sum);
