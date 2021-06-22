183ac412faa4db86955dfce3a795d7236f2046aa
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 16:34:18 +0000

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include "holberton.h"
-
 /**
- *  *  *  * main - Entry point
- *   *   *   *
- *    *    *    * Return: Always 0 (Success)
- *     *     *     **/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+ *print_alphabet - prints the alphabet
+ *
+ *Returns: nothing
+ *
+ */
 void print_alphabet(void)
 {
 	char a = 'a';
