3239ce6be847e80b3429523d512b05c203f09264
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-22 18:46:15 +0000

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - prints numbers
+ *
+ * @n: intiger value
+ *
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+	n = 0;
+	while (n <= 98)
+	{
+		_putchar(n);
+		n++;
+	}
+}
