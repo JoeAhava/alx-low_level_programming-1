e317e03d1a416374c1c577a8a571518ef1527538
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-24 18:53:44 +0000

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,12 @@
 
 void print_triangle(int size)
 {
-	for (int i = 0; i <= size; i++)
+	int i;
+	int j;
+
+	for (i = 0; i <= size; i++)
 	{
-		for (int j = size; j > 0; j++)
+		for (j = size; j > 0; j++)
 		{
 			_putchar(' ');
 		}
