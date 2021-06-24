926ea560225fff41b77fa04e10391aabb19a28e3
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-24 20:06:10 +0000

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if ((j / 10) > 0)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
