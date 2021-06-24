65fc9a6edb0fa93f5138e2ed847f95a229d70bb7
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-24 20:57:10 +0000

    update

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *  * positive_or_negative - Entry point
+ *   *
+ * @n: intiger
+ *    * Return: 0 for success
+ */
+void positive_or_negative(int n)
+{
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is positive\n", n);
+}
