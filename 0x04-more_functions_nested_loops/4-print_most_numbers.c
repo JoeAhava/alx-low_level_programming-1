f5a87e32531268f876c9aa1c91216067832ca1a0
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-24 20:07:54 +0000

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,12 @@
 
 void print_most_numbers(void)
 {
-	for (int i = '0'; i < '10'; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
-			_putchar(i);
+			_putchar('0' + (i % 10));
 	}
 	_putchar('\n')
 }
