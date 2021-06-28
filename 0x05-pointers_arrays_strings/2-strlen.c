e6936f81455d6f255834e75ccc0aa2e8fbb8c300
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 16:34:12 +0000

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - main function
+ *
+ * @s: pointer of a char
+ *
+ * Return: return value of function
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
