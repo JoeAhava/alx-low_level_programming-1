bcfa99141a667c79dc9e40796c65935012d34abb
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 17:10:49 +0000

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_rev - mai function
+ *
+ * @s: pointer of char
+ *
+ * Return: nothing
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(*(s + j);
+	}
+	_putchar('\n');
+
+}
