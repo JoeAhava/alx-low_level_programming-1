d4e779651d50d623c75d9ad6dcea5c01f768a2e7
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 20:45:23 +0000

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 			if (h < 0)
 				h = (h * 10) - (s[i] - '0');
 			else
-				j = (s[i] - '0') * -1;
+				h = (s[i] - '0') * -1;
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
