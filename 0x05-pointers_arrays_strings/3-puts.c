c8375cf25b70d77986255d3c7c6a72d4479cdad5
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 16:55:37 +0000

    update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts - main function
+ *
+ * @str: pointer of char
+ *
+ * Return: nothing
+ *
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
