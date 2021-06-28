e3e2fa388a6185f2da9b60f44b223c3df397bf77
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 21:02:37 +0000

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,12 +14,12 @@ int main(void)
 	int myrand, count, total;
 
 	srand(time(NULL));
-		for (count = 0, total = 2772; total > 122; count++)
-		{
-			myrand = (rand() % 125) + 1;
-			printf("%c" myrand);
-			total -= myrand;
-		}
-		printf("%c", total);
-		return (0);
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+	return (0);
 }
