93fed1cc0adb587944dbab0d6b1955a1c6ac5927
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-06-28 16:26:16 +0000

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * swap_int - main function
+ *
+ * @a: pointer of an intiger
+ *
+ * @b: pointer of an intiger
+ *
+ * Return: nothin
+ *
+ */g
+
+void swap_int(int *a, int *b)
+{
+	int tempo;
+
+	tempo = *a;
+	*b = *a;
+	*a = tempo;
+}
