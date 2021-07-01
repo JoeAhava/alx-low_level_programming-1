3dbbea07dfb4e31b588fd469a838c7c4f6066b7e
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-01 10:39:33 +0000

    updates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,8 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
 
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
 	return (dest);
 }
