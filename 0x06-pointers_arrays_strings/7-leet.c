ab465d783489c66f0a8c1b68b48df5e23777b651
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-01 18:21:50 +0000

    updates

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * leet - main func
+ * @c: char pointer
+ * Return: returns char
+ */
+
+char *leet(char *c)
+{
+	int i = 0;
+	int j = 0;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "43071"
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+				s[i] = b[j / 2];
+		}
+		return (c);
+}
