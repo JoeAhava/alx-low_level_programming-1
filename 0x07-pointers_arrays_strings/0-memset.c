d0a497c8eb2cb715dc760b4ca5661835797ea4e6
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-05 18:32:31 +0000

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 
-
+/**
+ * _memset - main function
+ * @s: char pointer var
+ * @b: char var
+ * @n: int var
+ * Return: char return
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i = 0;
 
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
 }
