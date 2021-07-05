1aa9d1e40d90fdf1069f51020baa9c96984bb50f
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-05 20:10:07 +0000

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - main func
+ * @s: char pointer var
+ * @accept: char pointer var
+ * Return: returns char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
