eca6f4774a4f642a6671805b387ef1aa5c0bda1e
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-05 19:23:30 +0000

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strchr - main func
+ * @s: char pointer var
+ * @c: char var
+ * Return: returns char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	while (j < i)
+	{
+		if (s[j] == c)
+		{
+			return (s);
+		}
+		else
+			return (NULL);
+		j++;
+	}
+}
