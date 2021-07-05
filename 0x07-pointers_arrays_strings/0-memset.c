a12ce8a677c16e85b113e324ba06bdbfa54d6d10
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-05 18:23:33 +0000

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+<b><em> This is the third part of more arrays <br>
+pointers and strings</em></b>
