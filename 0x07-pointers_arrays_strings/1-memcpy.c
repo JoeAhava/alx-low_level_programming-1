c7dff2eacb5c3317eb085532efb2f2e34db34c18
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-05 18:59:46 +0000

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - main func
+ * @dest: char pointer var
+ * @src: char pointer src
+ * @n: int var
+ * Return: returns char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+	return (dest);
+}
