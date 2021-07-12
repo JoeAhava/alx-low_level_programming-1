be7482a97dca42fbf6c676d5f5a59c842a1b4f31
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-12 17:13:09 +0000

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: num of arguments
+ * @argv: array of arguments
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
