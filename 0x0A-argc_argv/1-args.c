64798bb1b3f14060d86f246c8e248ca15a444351
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-12 17:22:14 +0000

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: numof arg
+ * @argv: value of arg
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc--);
+}
