24aedbe572a0609381fcd2bf5f413cd131243331
Eyosiyas Nigussie <116eyosi@gmail.com>
2021-07-13 18:12:52 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,8 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int s1count, s2count, sizeBuffer, i;
 
-	//if (s1 == NULL)
-		//s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	for (s1count = 0; s1[s1count]; s1count++)
